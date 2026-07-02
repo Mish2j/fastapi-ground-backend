dbb01b29c5c556d35ce8e33c5c6b4253f204ee4d
Mishel Ghukasyan <mishelghukasyan@gmail.com>
2026-07-01 18:01:46 -0700

    Add command endpoint for simulated spacecraft mode changes

--- app/main.py
+++ app/main.py
@@ -1,34 +1,69 @@
-from fastapi import FastAPI, WebSocket # FastAPI is a Python class that provides all the functionality for your API.
-from fastapi.responses import HTMLResponse
-from datetime import datetime
-import random
+# FastAPI is a Python class that provides all the functionality for your API.
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect, WebSocketException 
+# from fastapi.responses import HTMLResponse
 import asyncio
+from pydantic import BaseModel
 
-from telemetry import generate_telemetry
+from telemetry import generate_telemetry, get_telemetry_history as telemetry_history, get_latest_telemetry, set_mode
 
+
+class CommandRequest(BaseModel):
+    command: str
+    params: dict = {}
 
 app = FastAPI() # app variable will be an "instance" of the class FastAPI
 
-
+# ------------ GET ------------
 # path operation function
 @app.get("/") # tells FastAPI that the function right below is in charge of handling requests that go to the path /
 async def root():
     return {"message": "Hello World"}
 
+
 @app.get("/health")
 def health_check():
     return {"status": 'ok'}
 
-@app.get("/telemetry")
-def get_telemetry():
-    return generate_telemetry()
 
-# TODO: What is websocket used for?
+@app.get("/telemetry/latest")
+def get_telemetry_latest():
+    return get_latest_telemetry()
+
+
+@app.get("/telemetry/history")
+def get_telemetry_history():
+    return telemetry_history()
+    
+# ------------ POST ------------
+@app.post("/commands")
+def send_command(request: CommandRequest):
+    if request.command == "SET_MODE":
+        mode = request.params.get("mode")
+        return set_mode(mode)
+
+    return {
+        "status": "REJECTED",
+        "message": f"Unknown command: {request.command}"
+    }
+
+
+# ------------ websocket ------------
 @app.websocket("/ws/telemetry")
 async def tel_websocket(websocket: WebSocket):
     await websocket.accept()
 
-    while True:
-        data = generate_telemetry()
-        await websocket.send_json(data)
-        await asyncio.sleep(1)
+    try: 
+        while True:
+            data = generate_telemetry()
+            await websocket.send_json(data)
+            await asyncio.sleep(1)
+    except WebSocketDisconnect:
+        print("Client disconnected")
+
+"""
+GET /health
+GET /telemetry/latest
+GET /telemetry/history?point=battery_voltage&start=...&end=...
+WS  /ws/telemetry
+POST /commands
+"""
